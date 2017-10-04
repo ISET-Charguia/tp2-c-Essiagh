4432a020ae259886285203031bef510c070b1b37
essia ghoul <32401106+Essiagh@users.noreply.github.com>
2017-10-04 12:52:42 +0100

    Add files via upload

---
+++ TP2.ex6.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+ int main()
+
+ {
+
+  float min,max,a,b,c;
+
+ printf("taper les troi variables : \n");
+
+ scanf("%f" "%f" "%f",&a,&b,&c);
+
+ max=a;
+
+ min=a;
+
+ if (max<b)
+ {
+	 max=b;
+ }
+ if (max<c)
+ {
+	 max=c;
+ }
+ if (min>b)
+ {
+	 min=b;
+ }
+ if (min>c)
+ {
+	 min=c;
+ }
+return 0;
