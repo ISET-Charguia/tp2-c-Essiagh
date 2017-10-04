633e2d7ae2c0fe7b208a8877f1878a6902bed9a0
essia ghoul <32401106+Essiagh@users.noreply.github.com>
2017-10-04 12:50:32 +0100

    Add files via upload

---
+++ TP2.EX4.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : EX4.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a;
+
+	printf("donner l'année");
+	scanf("%d",&a);
+	if ('a%4=0')
+		printf("l'année %d est bissextile");
+	else printf("l'année %d n'est pas bissextile");
+	return 0;
+
+}
