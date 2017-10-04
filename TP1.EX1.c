40d4a6baaf2409cd5104b0019b45fa8b5d2dedb4
essia ghoul <32401106+Essiagh@users.noreply.github.com>
2017-10-04 12:47:01 +0100

    Add files via upload

---
+++ TP1.EX1.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+main()
+{
+	clrscr() ;
+	printf("bonjour 1fois\n");
+	printf("\t bonjour 2fois\n\n");
+	printf("bonjour 3fois\n");
+	printf("\n \bonjour 4fois\" \a");
+	printf("bonjour 1fois\n");
+	printf("bonjour 1fois\n");
+
+
+}
